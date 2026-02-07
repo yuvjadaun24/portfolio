da416e34ac0ed3fcc0750e74992bf48aa645ae69
yuvjadaun24 <yuvrajjadaun2@gmail.com>
2026-02-07 22:52:18 +0530

    Test fix

--- src/app/components/SplashScreen.tsx
+++ src/app/components/SplashScreen.tsx
@@ -112,7 +112,7 @@ export default function SplashScreen({ onSelect }: SplashScreenProps) {
             transform: 'skewX(-10deg)',
           }}
         >
-          DVD Test
+          DVD
         </div>
 
         <div
